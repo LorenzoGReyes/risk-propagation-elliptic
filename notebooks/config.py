9090ad7302308c1be194b878880b3e645a77d37e
lorenzoreyes <lorenzoreyes@etsy.com>
2026-03-22 01:20:46 +0000

    feat: adding plots notebook, started ml notebook

---
+++ notebooks/config.py
@@ -0,0 +1,4 @@
+BASE_PATH = "../data/elliptic_bitcoin_dataset/"
+FEATS_PATH = f"{BASE_PATH}elliptic_txs_features.csv"
+LABELS_PATH = f"{BASE_PATH}elliptic_txs_classes.csv"
+EDGES_PATH = f"{BASE_PATH}elliptic_txs_edgelist.csv"
